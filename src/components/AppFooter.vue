ca249ebd8f189622a712b8e783ceb5ec08802750
lorenzo-neri <lorenzo21neri@gmail.com>
2023-09-24 12:06:53 +0200

    add all footer with style

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -14,54 +14,165 @@ export default {
 </script>
 
 <template>
-    <div id="footer_top">
-        <div class="container">
-            <div class="row">
-                <div class="col-6 text-white">
-                    <div class="col-9 d-flex flex-wrap flex-column">
-                        <div class="col-3">
-                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. A molestiae repellat incidunt beatae
-                            quidem pariatur impedit odio minima numquam consectetur, similique at voluptatum ipsa iure quos
-                            placeat, veniam optio animi.
+    <div class="container min-vw-100">
+
+        <div class="row flex-column justify-content-flex-end dc-footerCont">
+
+            <footer class="p-0 ">
+
+                <div class="d-flex justify-content-center dc-footerLinks">
+
+                    <div class="col-8 d-flex dc-bigLogo">
+
+                        <div class="dc-listContainer">
+
+                            <h5 class="text-uppercase text-light">dc comics</h5>
+
+                            <ol class="list-unstyled text-light">
+                                <li class="text-capitalize"><a href="#">Character</a></li>
+                                <li class="text-capitalize"><a href="#">Comics</a></li>
+                                <li class="text-capitalize"><a href="#">Movies</a></li>
+                                <li class="text-capitalize"><a href="#">TV</a></li>
+                                <li class="text-capitalize"><a href="#">Games</a></li>
+                                <li class="text-capitalize"><a href="#">Videos</a></li>
+                                <li class="text-capitalize"><a href="#">News</a></li>
+                            </ol>
+
+                            <h5 class="text-uppercase text-light">shop</h5>
+
+                            <ol class="list-unstyled text-light">
+                                <li class="text-capitalize"><a href="#">Shop DC</a></li>
+                                <li class="text-capitalize"><a href="#">Shop DC Collectibles</a></li>
+                            </ol>
+
                         </div>
 
+                        <div class="dc-listContainer">
+
+                            <h5 class="text-uppercase text-light">dc</h5>
+
+                            <ol class="list-unstyled text-light">
+                                <li class="text-capitalize"><a href="#">terms of use</a></li>
+                                <li class="text-capitalize"><a href="#">privacy policy</a></li>
+                                <li class="text-capitalize"><a href="#">ad choices</a></li>
+                                <li class="text-capitalize"><a href="#">advertising</a></li>
+                                <li class="text-capitalize"><a href="#">jobs</a></li>
+                                <li class="text-capitalize"><a href="#">subscriptions</a></li>
+                                <li class="text-capitalize"><a href="#">talent workshops</a></li>
+                                <li class="text-capitalize"><a href="#">CPSC certificates</a></li>
+                                <li class="text-capitalize"><a href="#">ratings</a></li>
+                                <li class="text-capitalize"><a href="#">shop help</a></li>
+                                <li class="text-capitalize"><a href="#">contact us</a></li>
+                            </ol>
+
+                        </div>
+
+                        <div class="dc-listContainer">
+
+                            <h5 class="text-uppercase text-light">sites</h5>
+
+                            <ol class="list-unstyled text-light">
+                                <li class="text-capitalize"><a href="#">dc</a></li>
+                                <li class="text-capitalize"><a href="#">MAD magazine</a></li>
+                                <li class="text-capitalize"><a href="#">dc kids</a></li>
+                                <li class="text-capitalize"><a href="#">dc universe</a></li>
+                                <li class="text-capitalize"><a href="#">dc power visa</a></li>
+                            </ol>
+
+                        </div>
 
                     </div>
-                    <!-- /.col-9 -->
+
                 </div>
-                <!-- /.col-6 -->
-                <div class="col-6 d-flex justify-content-end">
-                    <div id="bg_logo" class="col-10 ">
+
+                <div class="d-flex justify-content-center dc-contacts">
+
+                    <div class="col-8 d-flex align-items-center justify-content-between">
+
+                        <button class="btn text-uppercase dc-signUpBtn">sign-up now!</button>
+
+                        <div class="d-flex align-items-center dc-followCont">
+
+                            <h4 class="text-uppercase m-0">follow us</h4>
+
+                            <a href="#" class="ms-3"><img src="../assets/img/footer-facebook.png" alt=""></a>
+                            <a href="#" class="ms-3"><img src="../assets/img/footer-twitter.png" alt=""></a>
+                            <a href="#" class="ms-3"><img src="../assets/img/footer-youtube.png" alt=""></a>
+                            <a href="#" class="ms-3"><img src="../assets/img/footer-pinterest.png" alt=""></a>
+                            <a href="#" class="ms-3"><img src="../assets/img/footer-periscope.png" alt=""></a>
+
+                        </div>
 
                     </div>
+
                 </div>
-                <!-- /.col-6 -->
-            </div>
-            <!-- /.row -->
+
+            </footer>
 
         </div>
 
     </div>
-    <!-- /#footer_top -->
-    <div id="footer_bottom"></div>
-    <!-- /#footer_bottom -->
 </template>
 
 <style lang="scss" scoped>
-#footer_top {
-    background-image: url(../../src/assets/img/footer-bg.jpg);
-    background-size: cover;
-    background-repeat: no-repeat;
+@use '../../src/assets/scss/app.scss' as *;
 
-    .row {
-        height: 400px;
+.dc-footerCont {
+    background-image: url("../../src/assets/img/footer-bg.jpg");
+    background-size: cover;
+
+}
+
+footer {
+    min-height: 325px;
+}
+
+.dc-footerLinks {
+
+    .dc-bigLogo {
+        background-image: url(../../src/assets/img/dc-logo-bg.png);
+        background-repeat: no-repeat;
+        background-position: right;
     }
 
-    #bg_logo {
-        background-image: url(../../src/assets/img/dc-logo-bg.png);
-        background-size: cover;
-        background-repeat: no-repeat;
-        background-position: center;
+    .dc-listContainer {
+
+        padding: 2rem 0;
+
+        margin-right: 2rem;
+
+        h5 {
+            font-weight: bold;
+        }
+
+        li a {
+            font-size: medium;
+            text-decoration: none;
+            color: $dc-secondary;
+
+            &:hover {
+                color: $dc-primary;
+            }
+        }
+    }
+
+}
+
+.dc-contacts {
+    padding: 1.5rem 0;
+    background-color: $dc-dark;
+
+    .dc-signUpBtn {
+        border: 3px solid $dc-primary;
+        border-radius: 0;
+        padding: 0.7rem 0.8rem;
+        font-weight: bold;
+        color: white;
+    }
+
+    h4 {
+        color: $dc-primary;
+        font-weight: bold;
     }
 }
 </style>
