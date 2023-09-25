70a994a40b00cf8daa4c4fe3fb3ab78141117fc4
lorenzo-neri <lorenzo21neri@gmail.com>
2023-09-25 12:02:14 +0200

    fix style tag a:hover in nav

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -12,16 +12,31 @@ export default {
 </script>
 
 <template>
-  <nav class="navbar">
-    <div class="container">
-      <a class="navbar-brand" href="#">
-        <img src="../assets/img/dc-logo.png" alt="DC Logo">
-      </a>
-      <div class="d-flex gap-4">
-        <a href="##" v-for="section in headerSections">{{ section }}</a>
-      </div>
+  <nav class="navbar py-0">
+    <a class="navbar-brand" href="#">
+      <img src="../assets/img/dc-logo.png" alt="DC Logo">
+    </a>
+    <div id="page" class="d-flex gap-4">
+      <a class="py-5" href="##" v-for="section in headerSections">{{ section }}</a>
     </div>
   </nav>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use '../../src/assets/scss/app.scss' as *;
+
+#page {
+  a {
+    text-decoration: none;
+    font-weight: bold;
+    color: $dc-secondary;
+    border-bottom: 5px solid transparent;
+    transition: .5s;
+
+    &:hover {
+      color: $dc-primary;
+      border-bottom: 5px solid $dc-primary;
+    }
+  }
+}
+</style>
